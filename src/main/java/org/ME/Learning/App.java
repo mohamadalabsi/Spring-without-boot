0449743903c5569b0fe74cedf1db5e1342d50581
mohamadalabsi <absimoda1@gmail.com>
2024-10-09 23:54:57 +0200

    constructor setter injection

--- src/main/java/org/ME/Learning/App.java
+++ src/main/java/org/ME/Learning/App.java
@@ -12,8 +12,24 @@ public class App
 
       ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml"); // this will create the container , as soon as the container created , it will look to the xml file and see the mentioned beans and create object for them
 //        Dev dev =context.getBean(Dev.class);  or
-//        Dev dev =(Dev) context.getBean("dev"); // (Dev) to make sure it (getBeans) returns the Object type
+        Dev dev =(Dev) context.getBean("dev"); // (Dev) to make sure it (getBeans) returns the Object type
 //        dev.build();
+
+
+//        dev.age=10;    1 look at dev class
+//        System.out.println(dev.age);
+
+//        dev.setAge(33);   2 , now it is private and assign the age var using normal setter
+//        System.out.println(dev.getAge());
+
+
+//        3  now the right way using spring , and assign the age var using spring
+//         System.out.println(dev.getAge());
+
+
+//        constructor injection
+        System.out.println(dev.getAge());
+
 
 
 
--- src/main/java/org/ME/Learning/Dev.java
+++ src/main/java/org/ME/Learning/Dev.java
@@ -5,9 +5,36 @@ import org.springframework.stereotype.Component;
 @Component
 public class Dev {
 
-    Dev(){
+             Dev(){
+
         System.out.println("Dev Constructor");
+  }
+
+//    constructor injection
+    public Dev(int age) {
+        this.age = age;
+        System.out.println("Dev 1 Constructor");
     }
+
+    private Laptop laptop;
+
+
+
+//    age should be private but for practice we let it like this and also to access it from main class
+//    int age ;  1
+
+//    now make it right    2, 3  , setter injection
+    private  int age;
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+
 
 
     public  void build(){
